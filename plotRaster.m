e6f090147334ff49b4f3060d9afcf1f5a3d97842
Noga Larry <noga.larry@mail.huji.ac.il>
2019-12-29 17:03:33 +0200

    no message

--- plotRaster.m
+++ plotRaster.m
@@ -1,7 +1,24 @@
-function plotRaster (raster, raster_params)
- ts = -raster_params.time_before:raster_params.time_after;
- raster = raster (raster_params.smoothing_margins+1: end -raster_params.smoothing_margins,:); 
- [x,y] = find(raster); x = ts(x);
- plot (x,y,'.'); hold on
- xlim ([-raster_params.time_before,raster_params.time_after])
- ylim ([1, size(raster,2)])
+function plotRaster (raster, raster_params, color)
+
+ts = -raster_params.time_before:raster_params.time_after;
+raster = raster (raster_params.smoothing_margins+1: end -raster_params.smoothing_margins,:);
+[x,y] = find(raster); x = ts(x);
+
+if  (mean(mean(raster))*1000)/size(raster,2) > 1.5
+    markerSize = 0.001;
+else
+    markerSize = 6;
+end
+
+if exist('color','var')
+    plot (x,y,['.' color ],'MarkerSize',markerSize); 
+else
+    plot (x,y,'.','MarkerSize',markerSize);
+end
+
+xlim ([-raster_params.time_before,raster_params.time_after])
+
+if (sum(sum(raster)))>0
+    ylim ([0, size(raster,2)])
+end
+
