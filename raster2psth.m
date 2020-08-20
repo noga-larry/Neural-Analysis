a83c30a4a036399ea68d5c9755cac909972fcc2b
nogalarry <noga.larry@mail.huji.ac.il>
2020-08-20 13:15:24 +0300

    raster2psth: deal with the case of empty raster

--- raster2psth.m
+++ raster2psth.m
@@ -11,13 +11,18 @@ function[psth] = raster2psth(raster,params)
 %                       will be shorters then raster as edges are removed 
 %                       to avoid edge artifacts. 
 
-psth = nanmean(raster',1);
-win = normpdf(-3*params.SD:3*params.SD,0,params.SD);
-win = win/sum(win);
-psth = filtfilt(win,1,psth);
-psth = psth*1000;
-psth = psth((params.smoothing_margins+1):(length(psth)-params.smoothing_margins));
+if size(raster,2)==0
+    disp('Empty raster!')
+    psth = nan(1,params.time_before+params.time_after+1);
+else
+    
+    psth = nanmean(raster',1);
+    win = normpdf(-3*params.SD:3*params.SD,0,params.SD);
+    win = win/sum(win);
+    psth = filtfilt(win,1,psth);
+    psth = psth*1000;
+    psth = psth((params.smoothing_margins+1):(length(psth)-params.smoothing_margins));
+end
 
-  
 end
 
