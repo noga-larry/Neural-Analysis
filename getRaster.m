ef2a7d89d92eb4136e07adbed11b20606d1ffe9e
nogalarry <noga.larry@mail.huji.ac.il>
2021-10-19 10:21:36 +0300

    Change getRaster to use a factory

--- getRaster.m
+++ getRaster.m
@@ -26,40 +26,24 @@ if ~any(strcmp(raster_params.align_to,{'all','allExtended'}))
         (raster_params.time_after+raster_params.smoothing_margins);
     display_time = raster_params.time_before + raster_params.time_after+1;
     raster = zeros (display_time+2*raster_params.smoothing_margins,length(ind));
+    alignment_times = alignmentTimesFactory(data,ind,raster_params.align_to);    
 end
 
 for f = 1:length(ind)
     
     switch raster_params.align_to
         
-        case 'cue'
-            raster (:,f) = times2Binary(data.trials(ind(f)).spike_times...
-                ,data.trials(ind(f)).cue_onset,...
-                window);            
-            
-        case 'targetMovementOnset'
-
-                raster (:,f) = times2Binary(data.trials(ind(f)).spike_times...
-                    ,data.trials(ind(f)).movement_onset,...
-                    window);
-                
         case 'reward'
-            if ~ isnan(data.trials(ind(f)).rwd_time_in_extended)
-                
-                raster (:,f) = times2Binary(data.trials(ind(f)).extended_spike_times...
-                    ,data.trials(ind(f)).rwd_time_in_extended,...
-                    window);
-            else
-                raster (:,f) = nan(length(window),1);
-                
-            end
+            raster (:,f) = times2Binary(data.trials(ind(f)).extended_spike_times...
+                ,alignment_times(f),...
+                window);
             
         case 'all'
             
             raster (:,f) = times2Binary(data.trials(ind(f)).spike_times...
                 ,0,...
                 1:data.trials(ind(f)).trial_length);
-        
+            
         case 'allExtended'
             len = data.trials(ind(f)).trial_length...
                 +EXTENDED_TIME_AFTER_TRIAL+...
@@ -67,6 +51,11 @@ for f = 1:length(ind)
             raster (:,f) = times2Binary(data.trials(ind(f)).extended_spike_times...
                 ,0,...
                 1:len);
+        
+        otherwise
+            raster (:,f) = times2Binary(data.trials(ind(f)).spike_times...
+                ,alignment_times(f),...
+                window);
     end
 end
 
